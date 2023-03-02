6118f28f12f65b151ce4039e435bb62bde19393c
fares <fares12yaseer45@gmail.com>
2023-03-02 17:08:07 +0200

    the main extention

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Project from './Project/Project'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Project />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Project/Project.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Style.scss'
+
+const Project = () => {
+    return (
+        <div className='container'>
+            <div className="logo"></div>
+            <div className="title"></div>
+            <div className="description"></div>
+            <div className="rate"></div>
+            <div className="btn"></div>
+        </div>
+    )
+}
+
+export default Project
