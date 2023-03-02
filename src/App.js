94115e6eb15f9efdc7b7a721e55320a225f8b0ea
fares <fares12yaseer45@gmail.com>
2023-03-02 18:35:15 +0200

    finish the first face

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Project from './Project/Project'
+import './App.css'
 
 const App = () => {
   return (
--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React  , {useState} from 'react'
 import './Style.scss'
+import Icon from './images/icon-star.svg'
+
+const circles = document.querySelectorAll(".circle")
+
+circles.forEach((item) => {
+    item.addEventListener('click' , function() {
+        this.classList.add('active')
+    })
+})
 
 const Project = () => {
+    const [content , setContent] = useState(true)
     return (
         <div className='container'>
-            <div className="logo"></div>
-            <div className="title"></div>
-            <div className="description"></div>
-            <div className="rate"></div>
-            <div className="btn"></div>
+            <div className="logo">
+                <div className="icon">
+                    <img src={Icon} alt="" />
+                </div>
+            </div>
+            <div className="title">
+                <h2>How Did We Do ?</h2>
+            </div>
+            <div className="description">
+                <p>
+                    Please Let Us Know How We Did With Your Support 
+                    Request. All FeedBack Is Appreciated To Help US
+                    Imporove Our Offering
+                </p>
+            </div>
+            <div className="rate">
+                <div className="circle">1</div>
+                <div className="circle">2</div>
+                <div className="circle">3</div>
+                <div className="circle">4</div>
+                <div className="circle">5</div>
+            </div>
+            <div className="btn">
+                <button>SUBMIT</button>
+            </div>
         </div>
     )
 }
